aa6e0d0c1a25b163f5dbdf76fc6ef73633d95f97
ryouta25 <avp22.07@mail.ru>
2025-11-10 20:45:27 +0300

    methods

--- main.cpp
+++ main.cpp
@@ -49,3 +49,29 @@ IntArray::IntArray(int i):
 {
     * a = i;
 }
+
+int IntArray::get(size_t id) const noexcept{
+  return a[id];
+}
+int IntArray::at(size_t id) const {
+  if (! id < k){
+    throw std::logic_error("no");
+  }
+  return a[id];
+}
+size_t IntArray::size() const noexcept {
+  return k;
+}
+int IntArray::last() const noexcept{
+  return get(size() - 1);
+}
+void IntArray::add(int i) {
+  int * tmp = new int[size() + 1];
+  for (size_t j = 0; j< size(); ++j) {
+    tmp[j] = a[j];
+  }
+  tmp[size()] = i;
+  delete[] a;
+  a = tmp;
+  ++k;
+}

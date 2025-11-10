7a5191d0e91a551d8c4c86dc4858336ecc416811
ryouta25 <avp22.07@mail.ru>
2025-11-10 20:37:42 +0300

    main update

--- main.cpp
+++ main.cpp
@@ -1,4 +1,28 @@
+#include<iostream>
+
 int main()
 {
-    
+  int next = 0;
+  std::cin >> next;
+  try {
+    IntArray a(next);
+    while (std::cin >> next)
+    {
+      a.add(next);
+    }
+    if (std::cin.fail()) {
+      return 1;
+    }
+
+    size_t count = 1;
+    for (size_t i = 0; i < a.size() - 1; ++i)
+    {
+      int d = a.get(i);
+      count+=!(d%a.last());
+    }
+    std::cout << count << std::endl;
+  } catch (...) {
+    return 2;
+  }
+  return 0;
 }
